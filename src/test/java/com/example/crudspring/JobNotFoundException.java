2ce0e8d9722d0efa9ecdf7cde8d1c1a757400c05
William Justin <williamjustinxu@gmail.com>
2022-09-06 00:57:22 +0700

    cleaning

--- src/test/java/com/example/crudspring/JobNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudspring;
-
-class JobNotFoundException extends RuntimeException {
-
-    JobNotFoundException(Long id) {
-        super(String.format("Not found %d", id));
-    }
-
-}
--- src/test/java/com/example/crudspring/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudspring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface JobRepository extends JpaRepository<Job, Long> {
-
-}
